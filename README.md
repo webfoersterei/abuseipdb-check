38d30f0f72be7a3f1828f535e3351042f1c4467f
Timo Förster <tfoerster@webfoersterei.de>
2018-05-30 14:16:55 +0200

    Base commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# AbuseIpDB checkscript
+
+Checks if an IP-adress is listed on abuseipdb.com and why. For usage with nagios / icinga.
+
+To use it periodically you need an API-Key from abuseipdb.com: https://www.abuseipdb.com/account#api-settings
+
+I am not into python at all - so this script lacks all code standards that may exist for python.
+
+## Requirements
+
+- Python3
+- PIP-Modules: urllib, simplejson
+
+## Usage
+
+```
+python3 check_abuseipdb.com -H '127.0.0.1' -K 'MYSECRETAPIKEY'
+```
+
+Example Output
+```
+CRITICAL - Reported for: Port Scan, Brute-Force, Web App Attack, SSH
+```
+
+If it's WARN or CRIT is determined by optional parameters `--warn` (default: 1) and `--crit` (default: 3) that configure thresholds how many reports were filed against you.
+
+Return code is:
+
+- OK = 0
+- WARN = 1
+- CRIT = 2
+- UNKNOWN = 3
---
+++ check_abuseipdb.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python3
+
+import sys
+import ipaddress
+import urllib.request
+import simplejson
+from optparse import OptionParser
+
+VERSION='0.0.3-alpha'
+USER_AGENT='abuseipdb_checkscript/%s (Python3/urllib; Github: webfoersterei)' % (VERSION)
+TIMEOUT=5
+BASEURL="https://www.abuseipdb.com/check/{}"
+
+# From https://www.abuseipdb.com/categories
+CATEGORY_NAMES={3: 'Fraud Orders', 4: 'DDoS Attack',
+                9: 'Open Proxy', 10: 'Web Spam',
+                11: 'Email Spam', 14: 'Port Scan',
+                18: 'Brute-Force',19: 'Bad Web Bot',
+                20: 'Exploited Host',21: 'Web App Attack',
+                22: 'SSH', 23: 'IoT Targeted'}
+
+# define exit codes
+EXIT_OK = 0
+EXIT_WARN = 1
+EXIT_CRIT = 2
+EXIT_UNKNOWN = 3
+
+def abuseip_check(opts):
+    exitCode = EXIT_UNKNOWN
+    apiurl = BASEURL + '/json?key={}'    
+    requestUrl = apiurl.format(opts.host, opts.key)
+    headers = {'User-Agent': USER_AGENT}
+    request = urllib.request.Request(requestUrl, None, headers)
+    try:
+        response = urllib.request.urlopen(request, timeout=TIMEOUT).read()
+    except Exception:
+        print('UNKNOWN - Problem querying API')
+        sys.exit(exitCode)
+    
+    entries = simplejson.loads(response)
+
+    if(len(entries) == 0):
+        print("OK - No entries found")
+        sys.exit(EXIT_OK)
+    elif(len(entries) > opts.criticalCount):
+        print('CRITICAL', end=' - ')
+        exitCode = EXIT_CRIT
+    elif(len(entries) > opts.warningCount):
+        print('WARN', end=' - ')
+        exitCode = EXIT_WARN
+
+    categories = set()
+    for entry in entries:
+        if len(entry['category']) > 0:
+            for cat in entry['category']:
+                categories.add(cat)
+    
+    categoryNames = []
+    for category in categories:
+        if category in CATEGORY_NAMES:
+            categoryNames.append(CATEGORY_NAMES[category])
+
+    print("Reported {0} times for: {1}".format(len(entries), ', '.join(categoryNames)))
+    sys.exit(exitCode)
+        
+
+
+def main():
+    parser = OptionParser("usage: %prog -H <IP address> -K <API key>")
+    parser.add_option("-H","--hostaddress", dest="host", help="Specify the IP address you want to check")
+    parser.add_option("-V","--version", action="store_true", dest="version", help="This option show the current version number of the program and exit")
+    parser.add_option("-K", "--key", dest="key", help="API-key for abuseipdb.com")
+    parser.add_option("-w", "--warn", dest="warningCount", default=1, type=int, help="Threshold for entries so that the return is a WARN")
+    parser.add_option("-c", "--crit", dest="criticalCount", default=3, type=int, help="Threshold for entries so that the return is a CRIT")
+
+    (opts, args) = parser.parse_args()
+    if opts.version:
+        print("check_abuseipdb.py %s"%VERSION)
+        sys.exit()
+    if not opts.key:
+        print("API-key is mandatory")
+        sys.exit()
+    if opts.host:
+        try:
+            ip = ipaddress.ip_address(opts.host)  
+        except ValueError:
+            parser.error("Incorrect IP Address.")
+        abuseip_check(opts)
+    else:
+        print("Hostaddress is mandatory")
+        sys.exit()
+ 
+if __name__ == '__main__':
+    main()
