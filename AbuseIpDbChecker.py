799a54f20de124ccd190e94113888553d86e0233
Timo Förster <tfoerster@webfoersterei.de>
2018-12-27 22:01:56 +0100

    Move apiLogic and entry-analyze to own class

---
+++ AbuseIpDbChecker.py
@@ -0,0 +1,66 @@
+import urllib.request
+import simplejson
+from AbuseIpDbCheckOptions import AbuseIpDbCheckOptions
+
+class AbuseIpDbChecker(object):
+    VERSION='1.0.0'
+    TIMEOUT=5
+    USER_AGENT='abuseipdb_checkscript/%s (Python3/urllib; Github: webfoersterei)' % (VERSION)
+    BASEURL="https://www.abuseipdb.com/check/{}"
+
+    # From https://www.abuseipdb.com/categories
+    CATEGORY_NAMES={3: 'Fraud Orders', 4: 'DDoS Attack',
+                    9: 'Open Proxy', 10: 'Web Spam',
+                    11: 'Email Spam', 14: 'Port Scan',
+                    18: 'Brute-Force',19: 'Bad Web Bot',
+                    20: 'Exploited Host',21: 'Web App Attack',
+                    22: 'SSH', 23: 'IoT Targeted'}
+    
+    # define exit codes
+    EXIT_OK = 0
+    EXIT_WARN = 1
+    EXIT_CRIT = 2
+    EXIT_UNKNOWN = 3
+
+    def queryEntriesFromApi(self, checkOptions: AbuseIpDbCheckOptions) -> object:
+        ''' Will get entries from API and return an python object with all entries '''
+        apiurl = self.BASEURL + '/json?key={}&days={}'    
+        requestUrl = apiurl.format(checkOptions.hostaddress, checkOptions.apiKey, checkOptions.daysToQuery)
+        headers = {'User-Agent': self.USER_AGENT}
+        request = urllib.request.Request(requestUrl, None, headers)
+        
+        response = urllib.request.urlopen(request, timeout=self.TIMEOUT).read()
+
+        return simplejson.loads(response)
+
+    def analyseEntries(self, checkOptions: AbuseIpDbCheckOptions, entries: list) -> int:
+        ''' Will print some information and return the exit code '''
+        
+        if(len(entries) == 0):
+            print("OK - No entries found")
+            return self.EXIT_OK
+        elif(len(entries) >= checkOptions.criticalThreshold):
+            print('CRITICAL', end=' - ')
+            exitCode = self.EXIT_CRIT
+        elif(len(entries) >= checkOptions.warningThreshold):
+            print('WARN', end=' - ')
+            exitCode = self.EXIT_WARN
+        else:
+            # EntryCount is not 0 but also no threshold was reached
+            print('OK', end=' - ')
+            exitCode = self.EXIT_OK
+
+        categories = set()
+
+        for entry in entries:
+            if len(entry['category']) > 0:
+                for cat in entry['category']:
+                    categories.add(cat)
+        
+        categoryNames = []
+        for category in categories:
+            if category in self.CATEGORY_NAMES:
+                categoryNames.append(self.CATEGORY_NAMES[category])
+
+        print("Reported {0}x (last {1}d) for: {2}".format(len(entries), checkOptions.daysToQuery, ', '.join(categoryNames)))
+        return exitCode
--- check_abuseipdb.py
+++ check_abuseipdb.py
@@ -2,71 +2,10 @@
 
 import sys
 import ipaddress
-import urllib.request
-import simplejson
+
 from optparse import OptionParser
+from AbuseIpDbChecker import AbuseIpDbChecker
 from AbuseIpDbCheckOptions import AbuseIpDbCheckOptions
-
-VERSION='0.1.0'
-USER_AGENT='abuseipdb_checkscript/%s (Python3/urllib; Github: webfoersterei)' % (VERSION)
-TIMEOUT=5
-BASEURL="https://www.abuseipdb.com/check/{}"
-
-# From https://www.abuseipdb.com/categories
-CATEGORY_NAMES={3: 'Fraud Orders', 4: 'DDoS Attack',
-                9: 'Open Proxy', 10: 'Web Spam',
-                11: 'Email Spam', 14: 'Port Scan',
-                18: 'Brute-Force',19: 'Bad Web Bot',
-                20: 'Exploited Host',21: 'Web App Attack',
-                22: 'SSH', 23: 'IoT Targeted'}
-
-# define exit codes
-EXIT_OK = 0
-EXIT_WARN = 1
-EXIT_CRIT = 2
-EXIT_UNKNOWN = 3
-
-def queryEntriesFromApi(checkOptions: AbuseIpDbCheckOptions) -> object:
-    ''' Will get entries from API and return an python object with all entries '''
-    apiurl = BASEURL + '/json?key={}&days={}'    
-    requestUrl = apiurl.format(checkOptions.hostaddress, checkOptions.apiKey, checkOptions.daysToQuery)
-    headers = {'User-Agent': USER_AGENT}
-    request = urllib.request.Request(requestUrl, None, headers)
-    
-    response = urllib.request.urlopen(request, timeout=TIMEOUT).read()
-
-    return simplejson.loads(response)
-
-def analyseEntries(checkOptions: AbuseIpDbCheckOptions, entries: list) -> int:
-    ''' Will print some information and return the exit code '''
-    if(len(entries) == 0):
-        print("OK - No entries found")
-        sys.exit(EXIT_OK)
-    elif(len(entries) >= checkOptions.criticalThreshold):
-        print('CRITICAL', end=' - ')
-        exitCode = EXIT_CRIT
-    elif(len(entries) >= checkOptions.warningThreshold):
-        print('WARN', end=' - ')
-        exitCode = EXIT_WARN
-    else:
-        # EntryCount is not 0 but also no threshold was reached
-        print('OK', end=' - ')
-        exitCode = EXIT_OK
-
-    categories = set()
-
-    for entry in entries:
-        if len(entry['category']) > 0:
-            for cat in entry['category']:
-                categories.add(cat)
-    
-    categoryNames = []
-    for category in categories:
-        if category in CATEGORY_NAMES:
-            categoryNames.append(CATEGORY_NAMES[category])
-
-    print("Reported {0}x (last {1}d) for: {2}".format(len(entries), checkOptions.daysToQuery, ', '.join(categoryNames)))
-    return exitCode
 
 def normalizeOptions(hostaddress, apiKey, warningThreashold, criticalThreshold, daysToQuery):
     return AbuseIpDbCheckOptions(hostaddress, apiKey, warningThreashold, criticalThreshold, daysToQuery)
@@ -83,22 +22,23 @@ def main():
     (opts, _) = parser.parse_args()
 
     if opts.version:
-        print("check_abuseipdb.py %s"%VERSION)
+        print("check_abuseipdb.py %s"%AbuseIpDbChecker.VERSION)
         sys.exit()
 
-    exitCode = EXIT_UNKNOWN
+    exitCode = AbuseIpDbChecker.EXIT_UNKNOWN
     checkOptions = normalizeOptions(opts.host, opts.key, opts.warningCount, opts.criticalCount, opts.days)
-    
+
     try:
-        entries = queryEntriesFromApi(checkOptions)
+        checker = AbuseIpDbChecker()
+        
+        entries = checker.queryEntriesFromApi(checkOptions)
         
         if(type(entries) == dict):
             entries = [entries]
         
-        exitCode = analyseEntries(checkOptions, entries)
+        exitCode = checker.analyseEntries(checkOptions, entries)
     except Exception as ex:
         print('UNKNOWN - Problem querying API: {0}'.format(ex))
-        sys.exit(EXIT_UNKNOWN)
 
     sys.exit(exitCode)
  
