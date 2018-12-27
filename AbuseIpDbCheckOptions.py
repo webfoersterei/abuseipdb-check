312db1b08323d6be45edf89fec0a8a09568ff2b2
Timo Förster <tfoerster@webfoersterei.de>
2018-12-27 20:28:08 +0100

    Add validation of hostaddress to OptionsClass

--- AbuseIpDbCheckOptions.py
+++ AbuseIpDbCheckOptions.py
@@ -1,3 +1,5 @@
+import ipaddress
+
 class AbuseIpDbCheckOptions(object):
 
     def __init__(self, hostaddress, apiKey, warningThreshold, criticalThreshold, daysToQuery):
@@ -10,4 +12,14 @@ class AbuseIpDbCheckOptions(object):
 
 
     def validate(self):
+        if(not self.hostaddress):
+            raise ValueError('Hostaddress must be set')
+        try:
+            ip = ipaddress.ip_address(self.hostaddress)  
+        except ValueError:
+            raise ValueError('Hostaddress must be a parsable address')
+
+        if(type(ip) != ipaddress.IPv4Address):
+            raise ValueError('Hostaddress must be a valid IPv4-address')
+    
         pass
--- tests.py
+++ tests.py
@@ -1,12 +1,25 @@
 #! /usr/bin/env python3
 import unittest
+
 from AbuseIpDbCheckOptions import AbuseIpDbCheckOptions
 
 class TestAbuseIpDbCheckOptions(unittest.TestCase):
 
-    def test_inistanciate(self):
-        options = AbuseIpDbCheckOptions(None, None, None, None, None)
+    def test_allValid(self):
+        options = AbuseIpDbCheckOptions('127.0.0.1', 'ashd7f38u4hfuebrfusda9wß', 2, 5, 14)
+        self.assertIsInstance(options, AbuseIpDbCheckOptions)
+    
+    def test_missingHostaddress(self):
+        with self.assertRaisesRegex(ValueError, 'Hostaddress must be set'):
+            AbuseIpDbCheckOptions(None, 'ashd7f38u4hfuebrfusda9wß', 2, 5, 14)
 
+    def test_notIpAtAllHostaddress(self):
+        with self.assertRaisesRegex(ValueError, 'Hostaddress must be a parsable address'):
+            AbuseIpDbCheckOptions('circusFooClown', 'ashd7f38u4hfuebrfusda9wß', 2, 5, 14)
+
+    def test_notValidIpv4Hostaddress(self):
+        with self.assertRaisesRegex(ValueError, 'Hostaddress must be a valid IPv4-address'):
+            AbuseIpDbCheckOptions('::1', 'ashd7f38u4hfuebrfusda9wß', 2, 5, 14)
 
 if __name__ == '__main__':
     unittest.main()
