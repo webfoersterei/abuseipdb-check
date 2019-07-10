ebfbdf46980c117dc29f5b93dfdd0ef772d3b513
Timo Förster <tfoerster@webfoersterei.de>
2019-07-10 22:50:20 +0200

    Switch to golang (surprise!): Implement APIv2

--- README.md
+++ README.md
@@ -2,34 +2,33 @@
 
 Checks if an IP-adress is listed on abuseipdb.com and why. For usage with nagios / icinga.
 
-To use it periodically you need an API-Key from abuseipdb.com: https://www.abuseipdb.com/account#api-settings
+To use it periodically you need an APIv2-Key from abuseipdb.com: https://www.abuseipdb.com/account/api
 
-I am not into python at all - so this script lacks all code standards that may exist for python.
-
-## Requirements
-
-- Python3
-- PIP-Modules: urllib, simplejson
+I am not into golang - so this script lacks all code standards that may exist.
 
 ## Usage
 
 ```
-python3 check_abuseipdb.py -H '127.0.0.1' -K 'MYSECRETAPIKEY'
+check_abuseipdb -key MYSECRETAPIKEY -host 127.0.0.1
 ```
 
 Parameters:
 ```
--V --version: show version and quit
--H --hostaddress: ip to check
--K --key: API-Key
--w --warn: Minimum reports to return a WARN (default: 1)
--c --crit: Minimum reports to return a CRIT (default: 3)
--d --days: Timespan to check in days (default: 14)
+-crit int
+    Minimum reports to return a CRIT (default 3)
+-days int
+    Timespan to check in days (default 14)
+-host string
+    Host to check
+-key string
+    abuseipdb APIv2 key
+-warn int
+    Minimum reports to return a WARN (default 1)
 ```
 
 Example Output
 ```
-CRITICAL - Reported 19x (last 14d) for: Port Scan, Brute-Force, Web App Attack, SSH
+CRITICAL - Found 4 entries from 3 users (Abuse Probability: 34%)
 ```
 
 Return code is:
---
+++ check_abuseipdb.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+const EXIT_CODE_OK = 0
+const EXIT_CODE_WARN = 1
+const EXIT_CODE_CRIT = 2
+const EXIT_CODE_UNKNOWN = 3
+
+type AbuseIpDbCheckResponse struct {
+	Data AbuseIpDbCheckData `json:"data"`
+}
+
+type AbuseIpDbCheckData struct {
+	IpAddress            string `json:"ipAddress"`
+	IsPublic             bool   `json:"isPublic"`
+	IpVersion            int    `json:"ipVersion"`
+	IsWhitelisted        bool   `json:"isWhitelisted"`
+	AbuseConfidenceScore int    `json:"abuseConfidenceScore"`
+	CountryCode          string `json:"countryCode"`
+	UsageType            string `json:"usageType"`
+	Isp                  string `json:"isp"`
+	Domain               string `json:"domain"`
+	TotalReports         int    `json:"totalReports"`
+	NumDistinctUsers     int    `json:"numDistinctUsers"`
+	LastReportedAt       string `json:"lastReportedAt"`
+}
+
+func buildStatusMessage(totalReports int, totalUsers int, abuseScore int) string {
+	return fmt.Sprintf("Found %d entries from %d users (Abuse Probability: %d%%)", totalReports, totalUsers, abuseScore)
+}
+
+func main() {
+	address := flag.String("host", "", "Host to check")
+	apiKey := flag.String("key", "", "abuseipdb APIv2 key")
+	daysToCheck := flag.Int("days", 14, "Timespan to check in days")
+	warnCount := flag.Int("warn", 1, "Minimum reports to return a WARN")
+	critCount := flag.Int("crit", 3, "Minimum reports to return a CRIT")
+
+	flag.Parse()
+
+	apiResult, err := getEntryCount(apiKey, address, daysToCheck)
+	if err != nil {
+		fmt.Println("UNKNOWN - Error: ", err)
+		os.Exit(EXIT_CODE_UNKNOWN)
+	}
+
+	statusMessage := buildStatusMessage(apiResult.TotalReports, apiResult.NumDistinctUsers, apiResult.AbuseConfidenceScore)
+
+	if apiResult.TotalReports >= *critCount {
+		fmt.Printf("CRITICAL - %s", statusMessage)
+		os.Exit(EXIT_CODE_CRIT)
+	} else if apiResult.TotalReports >= *warnCount {
+		fmt.Printf("WARNING - %s", statusMessage)
+		os.Exit(EXIT_CODE_WARN)
+	}
+
+	fmt.Printf("OK - %s", statusMessage)
+	os.Exit(EXIT_CODE_OK)
+}
+
+func getEntryCount(apiKey *string, address *string, daysToCheck *int) (AbuseIpDbCheckData, error) {
+	url := fmt.Sprintf("https://api.abuseipdb.com/api/v2/check?ipAddress=%s&maxAgeInDays=%d", *address, *daysToCheck)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return AbuseIpDbCheckData{}, err
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Key", *apiKey)
+	resp, err := client.Do(req)
+	if err != nil {
+		return AbuseIpDbCheckData{}, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return AbuseIpDbCheckData{}, err
+	}
+
+	response := AbuseIpDbCheckResponse{}
+	json.Unmarshal(body, &response)
+
+	return response.Data, err
+}
